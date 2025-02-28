626418d0281b2ea816f4fc18e5c3b678d59cab4a
YINGNAN <biancheng1688@163.com>
2025-02-28 20:26:03 +0800

    asd

--- src/ban/banEvent.py
+++ src/ban/banEvent.py
@@ -1,6 +1,11 @@
 from khl import Message, GuildUser, bot
 
-@bot.command(name='ban')
+""" @bot.command(name='ban')
 async def ban(msg : Message):
     user: GuildUser = msg.author
-    """ 你先研究下怎么弄更好，不然一堆if else史山了 """
+    你先研究下怎么弄更好，不然一堆if else史山了
+def check_permissions(is_student, is_employee):
+    status_dict = {(False, False): "无资格"}
+    return status_dict[(is_student, is_employee)]  
+
+print(check_permissions(False, False))  """
